fdd9dc6a7e749bebe641ed4deabd7e1c50f91574
Ali Naseri <mo.ali.naseri@gmail.com>
2024-03-27 19:45:18 +0330

    fix: change color of info item = null

--- libs/design/information/InfoItem.tsx
+++ libs/design/information/InfoItem.tsx
@@ -9,7 +9,7 @@ const InfoItem = ({ title, value }: InfoBoxProps) => {
   return (
     <li className="flex items-center overflow-hidden rounded-xl bg-gray-100 font-semibold">
       <span className="w-36 bg-gray-600 px-4 py-3 text-white">{title}</span>
-      <span className={twMerge('px-4', value ? '' : 'text-gray-600')}>
+      <span className={twMerge('px-4', value ? '' : 'text-gray-400/85')}>
         {value ?? 'وارد نشده'}
       </span>
     </li>
