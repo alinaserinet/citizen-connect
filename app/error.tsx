14d910ba96e7611101c5c7ce2d25268190718262
Ali Naseri <mo.ali.naseri@gmail.com>
2024-04-08 09:45:35 +0330

    fix: add error

--- app/error.tsx
+++ app/error.tsx
@@ -1,12 +1,11 @@
 'use client';
-import { useEffect } from 'react';
 
 interface ErrorProps {
   error: Error & { digest?: string };
   reset: () => void;
 }
 
-const Error = ({ error, reset }: ErrorProps) => {
+const Error = ({ error }: ErrorProps) => {
   console.log(error);
 };
 export default Error;
