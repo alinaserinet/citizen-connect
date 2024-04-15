f3b063bbb44badd9848aded562db8c9dd4f5776d
Ali Naseri <mo.ali.naseri@gmail.com>
2024-04-15 22:53:02 +0330

    fix: show dashboard by checking user role

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,13 @@
+import { authConfig } from '@config';
+import { jwtDecode } from 'jwt-decode';
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const session = await getServerSession(authConfig);
+  const { isEmployee } = jwtDecode<{ isEmployee: boolean }>(
+    session?.user.accessToken ?? '',
+  );
   return (
     <div className="grid grid-cols-1 gap-6 lg:grid-cols-4">
       <Link
@@ -9,12 +16,21 @@ const Dashboard = () => {
       >
         اطلاعات کاربری
       </Link>
-      <Link
-        href="/dashboard/requests"
-        className="rounded-xl bg-blue-300 p-4 text-center font-semibold"
-      >
-        درخواست مشکلات
-      </Link>
+      {!isEmployee ? (
+        <Link
+          href="/dashboard/requests"
+          className="rounded-xl bg-blue-300 p-4 text-center font-semibold"
+        >
+          درخواست مشکلات
+        </Link>
+      ) : (
+        <Link
+          href="/dashboard/requests"
+          className="rounded-xl bg-blue-300 p-4 text-center font-semibold"
+        >
+          مشاهده درخواست‌ های کاربران
+        </Link>
+      )}
     </div>
   );
 };
